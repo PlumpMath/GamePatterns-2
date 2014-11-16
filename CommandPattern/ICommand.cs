1f9c0a2533cf468d1b01292e8e4e7713c43390e4
Wikzo <Linkthewindwaker@gmail.com>
2014-11-16 19:40:18 +0100

    ICommand added

---
+++ CommandPattern/ICommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    interface ICommand
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+        void Run();
+    }
+}
